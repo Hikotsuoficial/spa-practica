8e4681bb40e6786aa539a68a28acd7a706f84e0b
Hikotsuoficial <derisccc@gmail.com>
2022-08-04 16:45:41 -0500

    working in responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
+<div class="general-container">
   <router-view/>
+</div>
 </template>
 
 <style lang="scss">
 @import '@/assets/styles/main.scss';
+
+.general-container{
+  width: 100%;
+  // border: var(--box-viewer);
+}
 </style>
--- src/components/views/HomeView.vue
+++ src/components/views/HomeView.vue
@@ -23,17 +23,17 @@
         </section>
       </div>
       <div class="wall-2" id="cards">
-        <section class="wall-content" >
+        <section class="wall-content">
           <h2 class="subtitle-text">Quien soy</h2>
           <div class="card-container-section">
             <section class="card-item">
-              <img src="" alt="">
+              <img src="" alt="" />
             </section>
             <section class="card-item">
-              <img src="" alt="">
+              <img src="" alt="" />
             </section>
             <section class="card-item">
-              <img src="" alt="">
+              <img src="" alt="" />
             </section>
           </div>
         </section>
@@ -71,56 +71,121 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
-@import '@/assets/styles/main.scss';
+@import "@/assets/styles/main.scss";
 
-.theme-blue{
-  display: grid;
+.theme-blue {
+  // display: grid;
+
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: 1fr;
+  }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+  @media screen and (max-width: 425px) {
+    grid-template-columns: 1fr;
+  }
+  @media screen and (max-width: 320px) {
+    grid-template-columns: 1fr;
+  }
 }
 
 .aside-menu {
-  height: 100%;
+  height: 100vh;
   width: 20%;
   background: var(--color-wall-1);
   position: fixed;
+  z-index: 12;
+
+  @media screen and (max-width: 1024px) {
+    height: 80px;
+    width: 100%;
+  }
 }
 
 .main-nav {
   height: 100%;
   width: 100%;
   @include center-items();
+  // justify-content: flex-end;
 }
 
-.main-menu{
+.main-menu {
   list-style: none;
-  padding: var(--padding-boxes);
+  width: 100%;
+  // padding: var(--padding-boxes);
 
-}
-
-.main-menu .nav-link{
-  font-size: 1.3rem;
-  margin-top: 2.1rem;
-  margin-left: 3rem;
-  text-align: right;
-  font-weight: 700;
-  border-bottom:2px solid var(--color-wall-1);
-  text-shadow: var(--text-shadow-1);
-  padding: 3px;
-
-  &:hover{
-    border-bottom:2px solid var(--color-wall-3);
+  @media screen and (max-width: 1024px) {
+    display: flex;
+    justify-content:center;
+    align-items: flex-end;
   }
 }
 
-.nav-link a{
+.main-menu .nav-link {
+  // margin-left: 3rem;
+  text-align: right;
+  font-weight: 700;
+  // text-shadow: var(--text-shadow-1);
+  padding: 18px;
+  border-bottom: 3px solid var(--color-wall-1);
+  transition: 0.3s;
+
+  &:hover {
+    background: linear-gradient(
+      90deg,
+      rgba(36, 104, 143, 1) 0%,
+      rgba(51, 54, 58, 1) 50%,
+      rgba(51, 54, 58, 1) 100%
+    );
+  }
+
+  @media screen and (max-width: 1024px) {
+  &:hover {
+    background: linear-gradient(
+      180deg,
+      rgba(36, 104, 143, 1) 0%,
+      rgba(51, 54, 58, 1) 50%,
+      rgba(51, 54, 58, 1) 100%
+    );
+  }
+  }
+}
+
+.nav-link a {
   text-decoration: none;
   color: var(--font-color-1);
+  // position: relative;
+  font-size: 1.2rem;
+  letter-spacing: 0.6px;
+
+  &::after {
+    content: "";
+    background: red;
+    height: 5px;
+    width: 10%;
+    right: 0;
+    bottom: -10px;
+    transition: 0.4s;
+  }
+
+  &:after::after {
+    width: 100%;
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
 }
 
 .info-section {
   margin-left: 20%;
   height: 100vh;
-  width: 80%;
   z-index: 10;
+
+  @media screen and (max-width: 1024px) {
+    margin-left: 0;
+  }
 }
 
 .wall-1 {
@@ -136,7 +201,7 @@ export default defineComponent({
   width: 100%;
   @include center-items();
   color: var(--font-color-1);
-  background-color: var(--color-wall-3);
+  background-color: var(--color-wall-4);
 }
 
 .wall-content {
@@ -144,29 +209,34 @@ export default defineComponent({
   width: 80%;
 }
 
-.title-text{
+.title-text {
   font-size: var(--titles-big);
   @include reset-padding-margin();
 }
 
-.subtitle-text{
-  font-size: var(--subttitles) ;
+.subtitle-text {
+  font-size: var(--subttitles);
 }
 
-.card-container-section{
+.card-container-section {
   border: var(--box-viewer);
-  padding: var(--padding-boxes);
   display: grid;
-  grid-template-columns: repeat(3, 310px);
+  grid-template-columns: repeat(3, 1fr);
   grid-template-rows: 1fr;
   justify-content: center;
   gap: var(--padding-boxes);
+
+  @media screen and (max-width: 1024px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media screen and (max-width: 768px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 }
 
-.card-item{
-  height: 250px;
+.card-item {
+  height: 33%;
   border: var(--box-viewer);
   border-radius: var(--border-radius);
 }
-
 </style>
